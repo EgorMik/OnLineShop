474838a2837fa38611f8e8eafcd5e3fcda66263c
Egor <egormikulic@gmail.com>
2021-01-27 14:54:58 +0300

    Adding Dto entity

--- OnLineShop/OnLineShop/Controllers/ProducsController.cs
+++ OnLineShop/OnLineShop/Controllers/ProducsController.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using OnLineShop.Core.Entities;
 using OnLineShop.Core.Interfaces;
 using OnLineShop.Core.Specifications;
+using OnLineShop.DTO;
 using OnLineShop.Infrastructure.Data;
 using System;
 using System.Collections.Generic;
@@ -31,20 +32,40 @@ namespace OnLineShop.Controllers
             _productTypeRepo = productTypeRepo;
         }
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts()
+        public async Task<ActionResult<List<ProductToReturnDto>>> GetProducts()
         {
             var spec = new ProductsWithTypesAndBrandsSpecification();
             var products = await _productRepo.ListAsync(spec);
 
-            return Ok(products);
+            return products.Select(product => new ProductToReturnDto
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Description = product.Description,
+                PictureUrl = product.PictureUrl,
+                Price = product.Price,
+                ProductBrands = product.ProductBrands.Name,
+                ProductTypes = product.ProductTypes.Name
+
+            }).ToList();
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Product>> GetProduct(int id)
+        public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
         {
             var spec = new ProductsWithTypesAndBrandsSpecification(id);
             var product = await _productRepo.GetEntityWithSpec(spec);
-            return Ok(product);
+            return new ProductToReturnDto
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Description = product.Description,
+                PictureUrl = product.PictureUrl,
+                Price = product.Price,
+                ProductBrands = product.ProductBrands.Name,
+                ProductTypes = product.ProductTypes.Name
+
+            };
         }
 
         [HttpGet("brands")]
---
+++ OnLineShop/OnLineShop/DTO/ProductToReturnDto.cs
@@ -0,0 +1,19 @@
+﻿using OnLineShop.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnLineShop.DTO
+{
+    public class ProductToReturnDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string PictureUrl { get; set; }
+        public string ProductTypes { get; set; }
+        public string ProductBrands { get; set; }
+    }
+}
